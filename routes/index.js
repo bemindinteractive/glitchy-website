0944ba89614f17c630c1bec49ae18f8fe0868e6a
Angelo Moroni <mor.angelo.mor@gmail.com>
2017-09-12 19:16:22 +0200

    font

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Glitchy | Glitch your f***ing world!' });
 });
 
 module.exports = router;
